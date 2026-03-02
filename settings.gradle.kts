8d9a9082df5d829e429de13e1f56e2537fe54d0b
Michael <58122000+michael-mezgo@users.noreply.github.com>
2026-03-02 20:42:40 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "CarRentalService"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,16 @@
+package at.ac.hcw.se
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureHTTP()
+    configureSecurity()
+    configureSerialization()
+    configureMonitoring()
+    configureDatabases()
+    configureRouting()
+}
