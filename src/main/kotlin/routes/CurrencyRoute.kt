b75766bb402105e33ea5ac3b6d9de12361544b75
ArAs101 <armin.assadzadeh@stud.hcw.ac.at>
2026-05-03 11:46:24 +0200

    Corrected currency route

--- src/main/kotlin/routes/CurrencyRoute.kt
+++ src/main/kotlin/routes/CurrencyRoute.kt
@@ -9,7 +9,7 @@ import io.ktor.server.routing.*
 
 fun Application.configureCurrencyRoutes() {
     routing {
-        route("/currency-service") {
+        route("/currencies") {
             get({}){
                 val result = CurrencyService.getSupportedCurrencies()
                 call.respond(HttpStatusCode.OK, result)
